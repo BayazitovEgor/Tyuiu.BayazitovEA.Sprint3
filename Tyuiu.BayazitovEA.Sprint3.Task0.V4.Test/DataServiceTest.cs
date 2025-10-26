84f3e35b7a401d10c94b0d980f43b3d4997b34eb
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-10-26 17:59:32 +0500

    Add project files.

---
+++ Tyuiu.BayazitovEA.Sprint3.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.BayazitovEA.Sprint3.Task0.V4.Lib;
+namespace Tyuiu.BayazitovEA.Sprint3.Task0.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 8;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            double wait = 6482.439;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint3.Task0.V4/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BayazitovEA.Sprint3.Task0.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int startValue = 1;
+        int stopValue = 8;
+
+        Console.WriteLine( ds.GetMultiplySeries(startValue, stopValue));
+
+
+
+
+    }
+}
