efa5e4b327d626676bc1cef47c6646568cbac3e0
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-10-27 17:24:25 +0500

    Task2

---
+++ Tyuiu.BayazitovEA.Sprint3.Task2.V15.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BayazitovEA.Sprint3.Task2.V15.Lib
+{
+    public class DataService : ISprint3Task2V15
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double multiplySeries = 0; ;
+            while (startValue <= stopValue)
+            {
+                multiplySeries = multiplySeries + ((Math.Pow(value, startValue) + 0.5) * Math.Cos(startValue));
+                startValue++;
+            }
+            return Math.Round(multiplySeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint3.Task2.V15/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.BayazitovEA.Sprint3.Task2.V15.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double value = 0.5;
+        int startValue = 1;
+        int stopValue = 20;
+
+        Console.WriteLine(ds.GetSumSeries(value, startValue, stopValue));
+    }
+}
