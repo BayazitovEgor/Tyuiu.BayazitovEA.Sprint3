ca573a6ce44420863bc36a3c2dbd8970930b3bcd
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-10-27 18:30:56 +0500

    Task3

---
+++ Tyuiu.BayazitovEA.Sprint3.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.BayazitovEA.Sprint3.Task3.V12.Lib;
+namespace Tyuiu.BayazitovEA.Sprint3.Task3.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string value = "bkkrk ckkkcs ksr";
+            char item = 'k';
+
+
+
+
+            double res = ds.GetMaxCharCount(value, item);
+
+            double wait =7;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint3.Task3.V12/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.BayazitovEA.Sprint3.Task3.V12.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        string value = "bkkrk ckkkcs ksr";
+        char item = 'k';
+
+
+
+        Console.WriteLine(ds.GetMaxCharCount(value, item));
+    }
+}
