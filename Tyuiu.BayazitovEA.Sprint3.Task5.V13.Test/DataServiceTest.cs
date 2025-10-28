af5dcf399e38d99d2d840d1f094fdbfec3b50eb1
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-10-28 23:56:47 +0500

    2

---
+++ Tyuiu.BayazitovEA.Sprint3.Task5.V13.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BayazitovEA.Sprint3.Task5.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint3.Task5.V13/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BayazitovEA.Sprint3.Task5.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x = 5;
+        int startValue1 = 1;
+        int startValue2 = 1;
+        int stopValue1 = 3;
+        int stopValue2 = 12;
+
+        Console.WriteLine(ds.GetSumSumSeries(x,startValue1, startValue2, stopValue1, stopValue2));
+    }
+}
